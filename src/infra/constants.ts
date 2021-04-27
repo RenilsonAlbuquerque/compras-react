a365d2eae78c74477d6b45f98298fe86e0c26541
Renilson Albuquerque <renilsonalbuquerque@gmail.com>
2021-04-27 17:20:40 -0300

    Adciona o link ao cors

--- src/infra/constants.ts
+++ src/infra/constants.ts
@@ -1,3 +1,4 @@
 //export const API_URL = 'http://192.168.0.105:8080/'
 export const API_URL = 'https://supermercado-back-node.herokuapp.com/'
-export const LOCALHOST_CORS = 'http://192.168.0.105'
+//export const LOCALHOST_CORS = 'http://192.168.0.105'
+export const LOCALHOST_CORS = 'https://wizardly-boyd-66ae23.netlify.app'
